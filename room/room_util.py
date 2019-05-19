14f64b6af079a50e9393072fb89b2f9d40867be7
Jack Kolb <jackhunterkolb@gmail.com>
2019-05-18 18:48:10 -0700

    working on room

--- room/room_util.py
+++ room/room_util.py
@@ -1,13 +1,63 @@
 import face_recognition
 import cv2
+import time
+import pyrebase
+import firebase_config
+
+
+firebase_config = firebase_config.set_firebase_config()
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
+token = auth.create_custom_token("room")
+user = auth.sign_in_with_custom_token(token)
+db = firebase.database()
+
+doctor_name = "NO DOCTOR"
+patient_name = "NO PATIENT"
 
 room_number = "NO ROOM NUMBER"
 
-
+# reads the "room" file to get the room number
 def get_room_number():
+    global room_number
     with open("room/room", "r") as room_number_file:
         room_number = room_number_file.readline()
     return room_number
+
+
+# gets the room information from firebase
+def get_room_information(room_number):
+    # retrieves from firebase
+    room_information = db.child("rooms").child("rooms").child(room_number)
+    if str(type(room_information)) == "<class 'NoneType'>":
+        return "free"
+
+    if room_information.child("doctor").get().val() == "":
+        return "free"
+    print(room_information.get().val())
+    doctor_name = room_information.child("doctor").get().val()
+    patient_id = room_information.child("patient").get().val()
+    return doctor_name, patient_id
+
+
+# get patient medical data from firebase
+def get_patient_medical(patient_id):
+    # retrieves from firebase
+    patient_medical_information = db.child("patient information").child(patient_id)
+    if str(type(patient_medical_information)) == "<class 'NoneType'>":
+        return "invalid id"
+    return patient_medical_information
+
+
+def periodicLoop():
+    while True:
+        doctor, patient_id = get_room_information(room_number)
+        print(str(doctor))
+        if doctor == "" or doctor == "NO DOCTOR" or doctor == None:
+            time.sleep(5)
+            continue
+        break
+    return
 
 
 def wait_for_doctor(doctor_name):
--- room/server.py
+++ room/server.py
@@ -1,40 +1,43 @@
 # SERVER FOR THE DOCTOR ROOM COMPUTERS!
 
 from flask import Flask, render_template, request, redirect
+from flask_socketio import SocketIO, emit
 import room_util
-import time
 import multiprocessing
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 @app.route("/eyes")
 def eyes():
     return render_template("eyes.html")
 
+@socketio.on('connect', namespace="/eye-catch")
+def eyecatch():
+    print("client connected - eyecatch")
+    room_util.wait_for_doctor("Sumanth Dara")
+    socketio.emit('continue', {'pass': True}, namespace='/eye-catch')
 
-@app.route("/medical", methods=["POST"])
+
+@socketio.on('connect', namespace="/periodic")
+def periodic():
+    print("client connected - periodic")
+    room_util.periodicLoop()
+    socketio.emit('continue', {'pass': True}, namespace='/periodic')
+
+
+@app.route("/medical")
 def medical():
-    if room_util.wait_for_doctor():
+    doctor_name = room_util.doctor_name
+    if room_util.wait_for_doctor(doctor_name):
         return render_template("medical.html")
     else:
         return redirect("/", code=302)
 
-
-def periodic():
-    while True:
-        
-        time.sleep(5)
-
 if __name__ == "__main__":
-    p = multiprocessing.Process(target=periodic)
-    p.start()
-
     room_util.get_room_number()
-
-
-    app.run()
+    app.run(port=4000)
